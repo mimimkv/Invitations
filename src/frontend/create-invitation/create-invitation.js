dec84a35af03739b8249a65c545582d5e47868b8
mimimkv <mimimkv11@gmail.com>
2023-01-12 17:10:42 +0200

    Add darkmode feature in login page and in create invitation page.

--- src/frontend/create-invitation/create-invitation.js
+++ src/frontend/create-invitation/create-invitation.js
@@ -57,3 +57,12 @@ async function createInvitation(data) {
     throw new Error(responseJson["error"]);
   }
 }
+
+const input = document.querySelector(".theme-switcher input");
+input.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    document.body.setAttribute("data-theme", "dark");
+  } else {
+    document.body.setAttribute("data-theme", "light");
+  }
+});
