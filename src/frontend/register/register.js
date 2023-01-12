3753975b16e8781335ad1adc7e416ea9bfdacc17
mimimkv <mimimkv11@gmail.com>
2023-01-12 23:18:55 +0200

    Add dark mode on registration page.

--- src/frontend/register/register.js
+++ src/frontend/register/register.js
@@ -55,3 +55,12 @@ async function saveUser(data) {
   }
 
 }
+
+const input = document.querySelector(".theme-switcher input");
+input.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    document.body.setAttribute("data-theme", "dark");
+  } else {
+    document.body.setAttribute("data-theme", "light");
+  }
+});
